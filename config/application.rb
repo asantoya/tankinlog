c4d4b9e0781098e30e6a336a9c7616248441cbc5
asantoya <armando.santoya@hotmail.com>
2012-08-08 15:50:59 -0500

    posible solution

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Estaciones
     config.filter_parameters += [:password]
 
     # Enable escaping HTML in JSON.
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
     config.active_support.escape_html_entities_in_json = true
     config.assets.compile = true
 

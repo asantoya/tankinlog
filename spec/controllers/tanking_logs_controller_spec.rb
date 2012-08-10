8f966d322dc37f7ae6a105590970ff6b02bd09f1
Asantoya <armando.santoya@hotmail.com>
2012-08-10 09:35:28 -0500

    Update spec/controllers/tanking_logs_controller_spec.rb
    a small correction

--- spec/controllers/tanking_logs_controller_spec.rb
+++ spec/controllers/tanking_logs_controller_spec.rb
@@ -5,7 +5,7 @@ describe TankingLogsController do
 		it "create a new tankin log for the user car" do
 			user = User.create!(:email => "user@example.com", :password => "foobar", :password_confirmation => "foobar" )
 			car = Car.create!(:brand => "example", :color => "foobar", :model => "foobar", :year =>"2012")
-      		visit new_user_car_path(:user_id => user.id,:car_id=>car.id)
+      		visit new_user_car_tanking_log_path(:user_id => user.id,:car_id=>car.id)
       		response.should be_success
     	end
     end

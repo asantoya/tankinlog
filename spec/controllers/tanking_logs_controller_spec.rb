47343f6ee1a0d456b9a730a8695edd8d999a11d0
Asantoya <armando.santoya@hotmail.com>
2012-08-10 16:21:24 -0500

    Update spec/controllers/tanking_logs_controller_spec.rb
    a better way to test the tanking_log controller

--- spec/controllers/tanking_logs_controller_spec.rb
+++ spec/controllers/tanking_logs_controller_spec.rb
@@ -5,19 +5,17 @@ describe TankingLogsController do
 		it "first visit the new tanking page" do
 			user =  Factory.create(:user)
 			car =  Factory.create(:car)
-      		visit new_user_car_tanking_log_path(:user_id => user.id,:car_id=>car.id)
-      		response.should be_success
+      			visit new_user_car_tanking_log_path(:user_id => user.id,:car_id=>car.id)
+      			response.should be_success
+    		end
     	end
-    end
-    describe "GET 'create'" do
-		before do
-    		user =  Factory.create(:user)
-			car =  Factory.create(:car)
-		end
-    	it "create the new tanking"do
-    	tanking=Factory.create(:tanking_log)
-    	visit user_car_tanking_logs_path(:user_id => user.id,:car_id=>car.id)
-    	response.should be_success
+    	describe "GET 'create'" do
+    		it "create the new tanking"do
+    			user =  Factory.create(:user)
+    			car =  Factory.create(:car)
+    			tanking=Factory.create(:tanking_log)
+    			visit user_car_tanking_logs_path(:user_id => user.id,:car_id=>car.id)
+    			response.should be_success
+    		end
     	end
-    end
 end
